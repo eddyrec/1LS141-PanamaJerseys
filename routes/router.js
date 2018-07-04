00333d0f57e02171ef46177f54d73471f5c0b572
Ubuntu 18.04 <eddy_rec@hotmail.com>
2018-07-04 16:01:37 -0500

    Puse lo de admin

--- routes/router.js
+++ routes/router.js
@@ -22,6 +22,10 @@ router.get('/login', function(req, res){
 	res.render('login');
 });
 
+router.get('/adminusr', function(req, res){
+	res.render('adminusr');
+});
+
 router.post('/login', function(req, res, next){
 	user.authenticate(req.body.email, req.body.password, function(error,user){
 		if(error)
@@ -32,67 +36,67 @@ router.post('/login', function(req, res, next){
 			next(err); }
 		else{
 			req.session.username = user.username;
-			res.redirect('/');  }
+			res.redirect('/adminusr');  }
 	});
 });
 
-router.get('/producto',function(req, res, next){
+router.get('/adminusr',function(req, res, next){
 	if(!req.session.username){
 		res.redirect('/login');
 	}
-	estudiante.findAll(function(error,users){
-		if(error)
-			next(error);
-		else if(!users)
-			users = [];
-		else
-			res.render('profile',{usuario:req.session.username, modelo:users});
-	}); 
+	// estudiante.findAll(function(error,users){
+	// 	if(error)
+	// 		next(error);
+	// 	else if(!users)
+	// 		users = [];
+	// 	else
+	// 		res.render('profile',{usuario:req.session.username, modelo:users});
+	// }); 
 });
 
-//INSERTAR
-router.post('/insertar', function(req, res, next){
-	estudiante.insert(req.body.nombre,req.body.apellido,req.body.edad,req.body.cedula,req.body.correo,req.body.carrera,req.body.year,req.body.direccion,req.body.sexo,req.body.indice, function(error,user){
-		if(error)
-			next(error);
-		else if(user){
-			var err = new Error('cedula ya existente');
-			err.status = 401;
-			next(err);}
-		else
-			res.redirect('/profile');
-	  });
-});
+// //INSERTAR
+// router.post('/insertar', function(req, res, next){
+// 	estudiante.insert(req.body.nombre,req.body.apellido,req.body.edad,req.body.cedula,req.body.correo,req.body.carrera,req.body.year,req.body.direccion,req.body.sexo,req.body.indice, function(error,user){
+// 		if(error)
+// 			next(error);
+// 		else if(user){
+// 			var err = new Error('cedula ya existente');
+// 			err.status = 401;
+// 			next(err);}
+// 		else
+// 			res.redirect('/profile');
+// 	  });
+// });
 
 //ACTUALIZAR
-router.post('/actualizar', function(req, res, next){
-	estudiante.update(req.body.nombre,req.body.apellido,req.body.edad,req.body.cedula,req.body.correo,req.body.carrera,req.body.year,req.body.direcion,req.body.sexo,req.body.indice, function(error,msg){
-		console.log(req.body.cedula);
-		if(error)
-			next(error);
-		else if(!msg){
-			var err = new Error('Usuario no existe');
-			err.status = 401;
-			next (err);}
-		res.redirect('/profile');
+// router.post('/actualizar', function(req, res, next){
+// 	estudiante.update(req.body.nombre,req.body.apellido,req.body.edad,req.body.cedula,req.body.correo,req.body.carrera,req.body.year,req.body.direcion,req.body.sexo,req.body.indice, function(error,msg){
+// 		console.log(req.body.cedula);
+// 		if(error)
+// 			next(error);
+// 		else if(!msg){
+// 			var err = new Error('Usuario no existe');
+// 			err.status = 401;
+// 			next (err);}
+// 		res.redirect('/profile');
 		
-	  });
-});
+// 	  });
+// });
 
-//ELIMINAR
-router.post('/eliminar', function(req, res, next){
-	estudiante.delete(req.body.cedula, function(error,msg){
-		if(error)
-			next(error);
-		else if(msg){
-			var err = new Error('cedula no existe');
-			err.status = 401;
-			next(err);
-		}
-		else{
-			console.log('exito');
-			res.redirect('/profile');}
-	  });
-});
+// //ELIMINAR
+// router.post('/eliminar', function(req, res, next){
+// 	estudiante.delete(req.body.cedula, function(error,msg){
+// 		if(error)
+// 			next(error);
+// 		else if(msg){
+// 			var err = new Error('cedula no existe');
+// 			err.status = 401;
+// 			next(err);
+// 		}
+// 		else{
+// 			console.log('exito');
+// 			res.redirect('/profile');}
+// 	  });
+// });
 
 module.exports = router;
